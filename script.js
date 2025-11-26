8efab3c70911695fc37529e04649b138c14530ca
Nabinur Islam Roni <nabinuri80@gmail.com>
2025-11-26 16:49:08 +0600

    new problem face

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 let passwordIcon = document.querySelector(".fa-eye")
 let usernametype = document.querySelector(".username input")
 let passwordtype = document.querySelector(".password input");
+let icon = document.querySelector('.sign-btn button span .fa-spinner');
+let checkboxtype = document.querySelector(".checkbox-area input")
 let userData;
 loadRoniAhmedJsonData();
 passwordIcon.addEventListener("click", () => {
@@ -29,15 +31,30 @@ let btn = document.querySelector(".btn-dark")
 let h1tag = document.querySelector("h1")
 
 btn.addEventListener("click", () => {
-    if (usernametype.value === "" && passwordtype.value === "") {
-        alert("Please enter both fields!")
-        h1tag.innerHTML = "Please enter username & password";
-    } else {
+     btn.textContent ='';
+     icon.style.display ='block'
+   
+   
+ 
+    if (usernametype.value === "" ) {
+        alert("Please enter userName fields!")
+        h1tag.innerHTML = "Please enter username";
+        usernametype.classList.add('errorPass')
+    } 
+    else if(passwordtype.value === ""){
+        alert("Please enter Password fields!")
+        h1tag.innerHTML = "Please enter password";
+        passwordtype.classList.add('errorPass')
+    }
+    else {
         let checkExisting = userData.find(x => x.userName == usernametype.value && x.password == passwordtype.value)
         if (checkExisting == null) {
             alert("Login Failed");
         } else {
-            alert("Login Success")
+            alert("Login Success");
+            localStorage.setItem('userName',usernametype.value);
+            localStorage.setItem('password',passwordtype.value);
+            localStorage.setItem('remembar',checkboxtype.value == 'on' ? 'true':'false');
         }
 
         //         userData.forEach((x ,i)=> {
@@ -62,4 +79,6 @@ btn.addEventListener("click", () => {
     //     h1tag.innerHTML="Login Fail";
     //     alert("Login Fail")
     // }
+
+  
 })
